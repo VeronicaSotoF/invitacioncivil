09895120c018c9bcae7754074528b0d26483c70e
VeronicaSotoF <sotofierroveronicadev@gmail.com>
2022-09-01 15:51:09 -0500

    first push

---
+++ src/ImageBanner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import bodaCivil from "./assets/bodaCivil.jpeg";
+import styled from "styled-components";
+
+const ImageBannerContainer = styled.div`
+  img {
+    max-width: 100%;
+  }
+`;
+
+const ImageBanner = () => {
+  return (
+    <ImageBannerContainer>
+      <img src={bodaCivil}></img>
+    </ImageBannerContainer>
+  );
+};
+
+export default ImageBanner;
